4bd0f3354a5790936a51f58fdd91b92eb2c67400
MurrayH13 <J074977@tafe.wa.edu.au>
2026-05-26 15:13:09 +0800

    minor edit

--- setup.py
+++ setup.py
@@ -27,19 +27,19 @@ transform = v2.Compose([
 
 
 def getdataloaders(batch_size):
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,                                            
+                                            download=True, transform=transform)
     
     
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)                                    download=True, transform=transform)
+                                            shuffle=True, num_workers=2)           
     return trainloader
 
 def gettestloaders(batch_size):
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
+                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
+                                            shuffle=False, num_workers=2)
     return testloader
 
 classes = ('plane', 'car', 'bird', 'cat',
